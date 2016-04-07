73c2a242d12493d6a864663a944f5c66dbdf27d6
Charles Moore <cmoore@blackmesh.com>
2016-04-07 09:06:05 -0700

    removing scripts

--- security_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PATH=$PATH:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/bmesh_admin/bin
-MAILTO="noc@blackmesh.com"
-MAILFROM="${HOSTNAME}"
-MESSAGE="See attached for security scan results"
-MAIL="mail -s 'Security scan for $MAILFROM' $EMAIL_ADDRESS < $MESSAGE"
-
-
